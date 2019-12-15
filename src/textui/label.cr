e9457199998fe5bd458163ebfcec66ba2bee7ebc
Hugo Parente Lima <hugo.pl@gmail.com>
2019-12-15 18:43:17 -0300

    Let TextUi::Label erase their area before render.

--- src/textui/label.cr
+++ src/textui/label.cr
@@ -2,8 +2,6 @@ module TextUi
   class Label < Widget
     property text
 
-    @old_text = ""
-
     def initialize(parent, @text : String = "")
       super(parent)
     end
@@ -12,17 +10,13 @@ module TextUi
       super(parent, x, y)
     end
 
-    def text=(text)
+    def text=(@text)
       invalidate
-      @old_text = @text
-      @text = text
     end
 
     def render
-      clear_text(0, 0, @old_text) unless @old_text.empty?
+      erase
       print_lines(0, 0, @text)
-
-      @old_text = ""
     end
   end
 end
